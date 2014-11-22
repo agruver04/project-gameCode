ae2f1b054f7955ad3916f48a237a8401599b92f3
agruver04 <agruver@g.hmc.edu>
2014-11-22 15:49:26 -0800

    first commit

---
+++ gamedata/sidedata.lua
@@ -0,0 +1,7 @@
+local sidedata = {
+	{
+	name = "TANKS",
+	startUnit = "bit",
+	}
+}
+return sidedata
---
+++ units/bit.lua
@@ -0,0 +1,79 @@
+local unitName = "bit"
+
+local unitDef = {
+	name= "bit",
+	unitname="bit",
+	Side = "TestSide",
+	
+	Description = "generic low range unit",
+	MaxDamage = 800,
+	
+	
+	objectName = "ball.s3o",
+	Acceleration = 1,
+    BrakeRate = 0.1,
+    FootprintX = 2,
+    FootprintZ = 2,
+    MaxSlope = 15,
+    MaxVelocity = 10,
+    MaxWaterDepth = 300,
+    MovementClass = "Default2x2",
+    TurnRate = 900,
+	
+	--Abilities
+    Builder = 0,
+    CanAttack = 1,
+    CanGuard = 1,
+    CanMove = 1,
+    CanPatrol = 1,
+    CanStop = 1,
+    LeaveTracks = 0,
+    Reclaimable = 0,
+	 
+	 weapons = {
+        [1] = {
+            def = "orangeblob",
+        },
+    },
+}
+
+local weaponDefs = {
+    orangeblob = {
+    name = "Orange Plasma Cannon",
+    weapontype = "Cannon",
+    accuracy = 10,
+    areaofeffect = 100,
+    avoidfeature = false,
+    avoidfriendly = true,
+    canattackground = true,
+    collidefriendly = true,
+    collisionsize = 8,
+    commandfire = false,
+    craterboost = 0,
+    cratermult = 0,
+    edgeeffectiveness = 0.1,
+    explosionspeed = 128,
+    impulseboost = 0,
+    impulsefactor = 0,
+    intensity = 1,
+    noselfdamage = true,
+    size = 4,
+--        soundstart = "tank_fire",
+--        soundhit = "explo01",
+    range = 250,
+    reloadtime = 1.5,
+    rgbcolor = "1.0 1.0 1.0",
+    turret = true,
+    texture1 = "flame",
+    weaponvelocity = 400,
+--		explosiongenerator = "custom:TANKGUN_FX",
+    damage =
+    {
+        default = 55,
+    },
+    },
+}
+
+unitDef.weaponDefs = weaponDefs
+unitDef.weaponDefs = weaponDefs
+return lowerkeys({ [unitName] = unitDef })
