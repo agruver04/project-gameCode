bc9f731b65bb107a38b4161b48076a96b375780e
agruver04 <agruver@g.hmc.edu>
2014-11-22 16:46:59 -0800

    all units are in the game, working on making them spawn now

---
+++ units/bug.lua
@@ -0,0 +1,78 @@
+local unitName = "bug"
+
+local unitDef = {
+	name= "bug",
+	unitname="bug",
+	Side = "TestSide",
+	
+	Description = "bug!",
+	MaxDamage = 800,
+	
+	
+	objectName = "bugNEW.s3o",
+	Acceleration = 1,
+    BrakeRate = 0.1,
+    FootprintX = 2,
+    FootprintZ = 2,
+    MaxSlope = 15,
+    MaxVelocity = 10,
+    MaxWaterDepth = 300,
+    MovementClass = "Default2x2",
+    TurnRate = 900,
+	
+	--Abilities
+    Builder = 0,
+    CanAttack = 1,
+    CanGuard = 1,
+    CanMove = 1,
+    CanPatrol = 1,
+    CanStop = 1,
+    LeaveTracks = 0,
+    Reclaimable = 0,
+	 
+	 weapons = {
+        [1] = {
+            def = "bugblob",
+        },
+    },
+}
+
+local weaponDefs = {
+    bugblob = {
+    name = "Orange Plasma Cannon",
+    weapontype = "Cannon",
+    accuracy = 10,
+    areaofeffect = 100,
+    avoidfeature = false,
+    avoidfriendly = true,
+    canattackground = true,
+    collidefriendly = true,
+    collisionsize = 8,
+    commandfire = false,
+    craterboost = 0,
+    cratermult = 0,
+    edgeeffectiveness = 0.1,
+    explosionspeed = 128,
+    impulseboost = 0,
+    impulsefactor = 0,
+    intensity = 1,
+    noselfdamage = true,
+    size = 4,
+--        soundstart = "tank_fire",
+--        soundhit = "explo01",
+    range = 250,
+    reloadtime = 1.5,
+    rgbcolor = "1.0 1.0 1.0",
+    turret = true,
+    texture1 = "flame",
+    weaponvelocity = 400,
+--		explosiongenerator = "custom:TANKGUN_FX",
+    damage =
+    {
+        default = 55,
+    },
+    },
+}
+
+unitDef.weaponDefs = weaponDefs
+return lowerkeys({ [unitName] = unitDef })
